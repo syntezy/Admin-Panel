2480b969d4c97596229bf57511634a37f0e961d5
Przemysław Sypniewski <p.sypniewski@onet.pl>
2018-10-09 16:05:40 +0200

    fix menu

--- js/script.js
+++ js/script.js
@@ -1,19 +1,7 @@
 'use strict' 
 
-var hamburgerMenu = document.querySelector('#hamburgerMenu');
-var logo = document.querySelector('.logo');
-var hiddenMenu = document.querySelector('.menu-to-hide');
-var container = document.querySelector('.container');
-var sideBar = document.querySelector('.side-bar');
-
 $(document).ready(function(){
     $('#hamburgerMenu').click(function(){
-        $('.logo').toggleClass('logoToggle');
-    })
-    $('#hamburgerMenu').click(function(){
-        $('.hiddenMenu').toggleClass('hiddenMenuToggle');
-    })
-    $('#hamburgerMenu').click(function(){
-        $('.container').toggleClass('containerToggle');
+        $('.side-bar').toggleClass('toggle');
     })
 })
